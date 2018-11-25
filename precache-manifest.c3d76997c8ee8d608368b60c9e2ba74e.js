9d3a0dde0fb25027fa4836c5845d4a4d196f551a
SSokolan <sokolan.sergey@gmail.com>
2018-11-25 16:22:58 +0300

    Updates

---
+++ precache-manifest.c3d76997c8ee8d608368b60c9e2ba74e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86ddadfa0734a0bfe548",
+    "url": "/vs-markup/static/js/runtime~main.86ddadfa.js"
+  },
+  {
+    "revision": "a7961c8574d3e003c317",
+    "url": "/vs-markup/static/js/main.a7961c85.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/vs-markup/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "a7961c8574d3e003c317",
+    "url": "/vs-markup/static/css/main.c1badaa8.chunk.css"
+  },
+  {
+    "revision": "1684c82ae427b450db281f201292b15b",
+    "url": "/vs-markup/index.html"
+  }
+];
