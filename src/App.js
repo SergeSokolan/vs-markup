c43583484322dee3e4cbf0e89d991ae0468a744d
SSokolan <sokolan.sergey@gmail.com>
2018-12-01 14:09:49 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -9,13 +10,30 @@ class App extends Component {
           <h1 className="form__title">Welcome to Voipscan</h1>
           <span className="form__text">Enter your phone number</span>
           <input className="form__input" type="text" />
-          <input className="form__button" type="button" value="Send code" />
+          <Link to="/chat" className='form__link'>
+            <input className="form__button" type="button" value="Send code" />
+          </Link>
         </div>
         <div className="login__social-links">
           <ul className="social-links__list">
-            <li className="social-links__item"><a className='social-links__link social-links__link_fb' href='#'></a></li>
-            <li className="social-links__item"><a className='social-links__link social-links__link_tw' href='#'></a></li>
-            <li className="social-links__item"><a className='social-links__link social-links__link_vk' href='#'></a></li>
+            <li className="social-links__item">
+              <a
+                className="social-links__link social-links__link_fb"
+                href="#"
+              />
+            </li>
+            <li className="social-links__item">
+              <a
+                className="social-links__link social-links__link_tw"
+                href="#"
+              />
+            </li>
+            <li className="social-links__item">
+              <a
+                className="social-links__link social-links__link_vk"
+                href="#"
+              />
+            </li>
           </ul>
         </div>
       </section>
---
+++ src/Chat.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Chat extends Component {
+  render() {
+    return (
+     
+        <h1>Hello, router!</h1>
+
+    );
+  }
+}
+
+export default Chat;
