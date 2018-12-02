e492a7160d855f48c1a775bb828420c72d761795
SSokolan <sokolan.sergey@gmail.com>
2018-12-02 12:20:30 +0300

    markup

--- src/Chat.js
+++ src/Chat.js
@@ -7,7 +7,7 @@ class Chat extends Component {
         <div className="chat__contacts">
           <div className="chat-head">
             <span className="user-icon">
-              <img src="./images/user.jpg" alt="pic" />
+              <img src="/images/user.jpg" alt="pic" />
             </span>
             <div className="chat-head__controls">
               <span className="search" />
@@ -17,19 +17,25 @@ class Chat extends Component {
           <div className="chat-body">
             <div className="chat-item">
               <span className="user-icon">
-                <img src="./images/user.jpg" alt="pic" />
+                <img src="/images/user.jpg" alt="pic" />
               </span>
               <div className="chat-message">
-                <span className="username username_gray">Сергей</span>
-                <p className="message message_last">
-                  Да, я понял. Я забыл вчера ключиот гаража – так что уже
-                  вернусь домой за ключами, заберу велик и прикачу сразу
-                </p>
-              </div>
-              <div className="chat-info">
-                <span className="delivery-status" />
-                <time className="send-time">16:56</time>
-                <span className="unread-msg-counter">888</span>
+                <div className="chat-message__status">
+                  <span className="username username_gray">Сергей</span>
+                  <span className="delivery-status" />
+                  <time className="send-time">16:56</time>
+                </div>
+
+                <div className="chat-message__body">
+                  <div>
+                    <span className="you">Вы</span>
+                    <p className="message message_last">
+                      Да, я понял. Я забыл вчера ключиот гаража – так что уже
+                      вернусь домой за ключами, заберу велик и прикачу сразу
+                    </p>
+                  </div>
+                  <span className="unread-msg-counter">888</span>
+                </div>
               </div>
             </div>
           </div>
@@ -37,7 +43,7 @@ class Chat extends Component {
         <div className="chat__messages">
           <div className="chat-head chat-head_light-color">
             <span className="user-icon">
-              <img src="./images/user.jpg" alt="pic" />
+              <img src="/images/user.jpg" alt="pic" />
             </span>
             <div className="chat-head__controls">
               <span className="search" />
@@ -47,12 +53,12 @@ class Chat extends Component {
           <div className="chat-body">
             <div className="chat-item">
               <span className="user-icon">
-                <img src="./images/user.jpg" alt="pic" />
+                <img src="/images/user.jpg" alt="pic" />
               </span>
               <div className="chat-message">
                 <span className="username">Сергей</span>
                 <p className="message">
-                  Да, я понял. Я забыл вчера ключиот гаража – так что уже
+                  Да, я понял. Я забыл вчера ключи от гаража – так что уже
                   вернусь домой за ключами, заберу велик и прикачу сразу
                 </p>
               </div>
